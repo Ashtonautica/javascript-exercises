5fc7c5dbf6274cf94f4992eaec711cf1b39659a2
Ashtonaut <ashtonautica@gmail.com>
2023-12-14 15:35:39 +0000

    Finished solution 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundTo1DP = function(input) {
+  return Math.round(input * 10) / 10;
+}
+
+const convertToCelsius = function(tempF) {
+  return roundTo1DP((tempF - 32) / 1.8);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return roundTo1DP((tempC * 1.8) + 32);
 };
 
 // Do not edit below this line
