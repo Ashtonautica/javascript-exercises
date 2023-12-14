a245c06c2c59d4ede1a008552fe4de30078aceff
Ashtonaut <ashtonautica@gmail.com>
2023-12-14 15:20:27 +0000

    Finished solution 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumToN = function(n) {
+    return 0.5 * n * (n+1);
+}
 
+const sumAll = function(x, y) {
+    if ((x < 0) || (y < 0)) return "ERROR"
+    if ((typeof x !== "number") || (typeof y !== "number")) return "ERROR"
+
+    let largest = (x > y) ? x : y;
+    let smallest = (x > y)? y : x;
+    return sumToN(largest) - sumToN(smallest - 1);
 };
 
 // Do not edit below this line
