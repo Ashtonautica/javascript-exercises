1a1e39cf485c7d07d742d0cdb23ba4de2243c716
Ashtonaut <ashtonautica@gmail.com>
2023-12-14 13:51:37 +0000

    Finished solution 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    let charArray = string.split("");
+    while (charArray.length > 0) {
+        output += charArray.pop();
+    }
+    return output;
 };
 
 // Do not edit below this line
