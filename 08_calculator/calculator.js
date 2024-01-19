f25b7c42f81a1e0ecc07bd79312f674e37ae183c
Ashtonaut <ashtonautica@gmail.com>
2024-01-19 16:39:48 +0000

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = base;
+	for (let n = 1; n < exponent; n++) {
+    result = result * base;
+  };
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let number = n;
+  let result = 1;
+  while (number > 0) {
+    result = result * number;
+    number -= 1;
+  }
+  return result;
 };
 
 // Do not edit below this line
