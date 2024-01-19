f1f314faba15a203f796a0624d6f9ecfcf341d1a
Ashtonaut <ashtonautica@gmail.com>
2024-01-19 17:15:52 +0000

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let formattedText = text
+        .replace(/[!,. ]/g, "")
+        .toLowerCase();
+    console.log(formattedText);
+    return formattedText === formattedText.split("").reverse().join("");
 };
 
 // Do not edit below this line
